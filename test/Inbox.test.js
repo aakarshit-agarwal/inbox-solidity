4a44f7b8cb1208fd17998020687f18c1667b8757
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-10 00:12:50 +0530

    Using async await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,12 +4,13 @@ const { Web3 } = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
-    web3.eth.getAccounts().then((account) => {
-        console.log(account);
-    });
+let accounts;
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {});
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
 });
